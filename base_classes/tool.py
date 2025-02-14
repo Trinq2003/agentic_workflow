ad8f0387cc9b8fb1201003424995b1b8d7b3fd36
Trinq2003 <tri.nq210860@sis.hust.edu.vn>
2025-02-15 01:20:09 +0700

    Add class id reference retrieval for Tools

--- base_classes/tool.py
+++ base_classes/tool.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, List, Dict, Self
 import logging
 
 from configuration.tool_configuration import ToolConfiguration
@@ -10,6 +10,8 @@ class AbstractTool(ABC):
     """
     _config: ToolConfiguration = None
     _tool_id: str = None
+    _list_of_tool_ids: List[str] = None
+    _tool_instances_by_id: Dict[str, Self] = None
     
     _webhook_base_url: str = None
     _webhook_webhook_path: str = None
@@ -33,6 +35,28 @@ class AbstractTool(ABC):
         self._headers_content_type: str = self._config.headers_content_type
         self._headers_authorization: str = self._config.headers_authorization
         self._tool_id: str = self._config.tool_id
+        
+        self.__class__._list_of_tool_ids.append(self._tool_id)
+        self.__class__._tool_instances_by_id[self._tool_id] = self
+        
+    @classmethod
+    def get_tool_ids(cls) -> List[str]:
+        """
+        Get the list of operator IDs.
+
+        :return: The list of operator IDs.
+        :rtype: str
+        """
+        return cls._list_of_tool_ids
+    @classmethod
+    def get_tool_instance_by_id(cls, tool_id) -> Self:
+        """
+        Retrieve an instance of the class by its ID.
+
+        :param id: The unique identifier of the instance.
+        :return: The instance if found, otherwise None.
+        """
+        return cls._tool_instances_by_id.get(tool_id, None)
 
     def load_config(self, tool_config: ToolConfiguration) -> None:
         """
