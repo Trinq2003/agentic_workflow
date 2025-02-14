27b46ad28ad787d8587cff5faca303f6c033d693
Trinq2003 <tri.nq210860@sis.hust.edu.vn>
2025-02-15 01:21:18 +0700

    Commit Abstract Code for Operator class

--- base_classes/operator.py
+++ base_classes/operator.py
@@ -1,25 +1,75 @@
 from abc import ABC, abstractmethod
-from typing import List, Dict, Union, Any, Iterable
+from typing import List, Dict, Union, Any, Iterable, Self
 
 from base_classes.llm import AbstractLanguageModel
 from base_classes.tool import AbstractTool
+from configuration.operator_configuration import OperatorConfiguration
 
 class AbstractOperator(ABC):
     """
     Abstract base class that defines the interface for all operators.
     """
-    operator_id: str = None
-    _llm_component: Iterable[AbstractLanguageModel] = None
-    _tool_component: Iterable[AbstractTool] = None
-    @abstractmethod
-    def __init__(self, config: Dict[str, Any]) -> None:
+    _config: OperatorConfiguration = None
+    _operator_id: str = None
+    _operator_type: str = None
+    _enabled: bool = None
+    _list_of_operator_ids: List[str] = None
+    _operator_instances_by_id: Dict[str, Self] = None
+    
+    _llm_component: List[AbstractLanguageModel] = None
+    _tool_component: List[AbstractTool] = None
+    _execution_timeout: int = None
+    _execution_max_retry: int = None
+    _execution_backoff_factor: int = None
+    def __init__(self, config: OperatorConfiguration) -> None:
         """
         Initialize the AbstractOperator instance with configuration.
 
         :param config: The operator configuration object.
         :type config: Dict[str, Any]
         """
-        pass
+        self._config = config
+        
+        self._operator_id = self._config.operator_id
+        self._operator_type = self._config.operator_type
+        self._enabled = self._config.operator_enabled
+        self._execution_timeout = self._config.execution_timeout
+        self._execution_max_retry = self._config.execution_max_retry
+        self._execution_backoff_factor = self._config.execution_backoff_factor
+        
+        str_llm_component = self._config.operator_llm_component
+        list_of_initiated_llm = AbstractLanguageModel.get_llm_ids()
+        for llm_component in str_llm_component:
+            if llm_component in list_of_initiated_llm:
+                self._llm_component.append(AbstractLanguageModel.get_llm_instance_by_id(llm_id = llm_component))
+        
+        str_tool_component = self._config.operator_tool_component
+        list_of_initiated_tool = AbstractTool.get_tool_ids()
+        for tool_component in str_tool_component:
+            if tool_component in list_of_initiated_tool:
+                self._tool_component.append(AbstractTool.get_tool_instance_by_id(tool_id = tool_component))
+        
+        self.__class__._list_of_operator_ids.append(self._operator_id)
+        self.__class__._operator_instances_by_id[self._operator_id] = self
+        
+    @classmethod
+    def get_operator_ids(cls) -> List[str]:
+        """
+        Get the list of operator IDs.
+
+        :return: The list of operator IDs.
+        :rtype: str
+        """
+        return cls._list_of_operator_ids
+    @classmethod
+    def get_operator_instance_by_id(cls, operator_id) -> Self:
+        """
+        Retrieve an instance of the class by its ID.
+
+        :param id: The unique identifier of the instance.
+        :return: The instance if found, otherwise None.
+        """
+        return cls._operator_instances_by_id.get(operator_id, None)
 
     @abstractmethod
     def run(self, data: Dict[str, Any]) -> Dict[str, Any]:
---
+++ configuration/operator_configuration.py
@@ -0,0 +1,35 @@
+from typing import List, Dict, Union, Any, Optional
+
+from base_classes.configuration import Configuration
+
+class OperatorConfiguration(Configuration):
+    """
+    Base configuration class for operator configurations.
+    Contains common properties for both local and API-based deployments.
+    """
+    operator_id: str
+    operator_type: str
+    operator_enabled: bool
+    operator_llm_component: List[str]
+    operator_tool_component: List[str]
+    execution_timeout: int
+    execution_max_retry: int
+    execution_backoff_factor: int
+    def __init__(self):
+        super().__init__()
+        self.sensitive_properties = []
+
+    def _init_properties(self):
+        """
+        Define common properties for operator.
+        """
+        return [
+            ['operator.operator_id', '', str], # Operator ID
+            ['operator.operator_type', 'CoT', str], # Operator type
+            ['operator.enabled', True, bool], # Operator enabled
+            ['operator.llm_component', [], list], # LLM component
+            ['operator.tool_component', [], list] # Tool component
+            ['execution.timeout', 60, int] # Execution timeout
+            ['execution.max_retry', 3, int] # Execution retry
+            ['execution.backoff_factor', 2, int] # Execution backoff
+        ]
