48cfc6ec2ceff5785d9249ccbdbdc123375833e2
Trinq2003 <tri.nq210860@sis.hust.edu.vn>
2025-02-21 23:26:02 +0700

    Remove permission and access matrix from MemoryBlock

--- base_classes/memory/memory_block.py
+++ base_classes/memory/memory_block.py
@@ -6,9 +6,9 @@ from functools import lru_cache
 import time
 
 from base_classes.memory.memory_atom import AbstractMemoryAtom
-from base_classes.memory.management_term import AccessPermission
 from base_classes.prompt import AbstractPrompt
 from base_classes.system_component import SystemComponent
+from base_classes.memory.management_term import MemoryType
 
 class AbstractMemoryBlock(ABC):
     """
@@ -21,15 +21,15 @@ class AbstractMemoryBlock(ABC):
     """
     _mem_block_id: uuid.UUID
     _memory_atoms: List[AbstractMemoryAtom] = []
-    _block_access_matrix: Dict[str, Dict[str, AccessPermission]] = {} # Access matrix for different system components per each memory atom
     _mem_atom_graph: Dict[uuid.UUID, List[uuid.UUID]] = {} # Graph of memory atoms and their dependencies
+    _block_memory_type: MemoryType
     
     _list_of_memblock_ids: List[uuid.UUID] = []
     _memblock_instances_by_id: Dict[str, Self] = {}
-    def __init__(self):
+    def __init__(self, block_memory_type: MemoryType):
         self._mem_block_id: uuid.UUID = uuid.uuid4()
         self._memory_atoms: List[AbstractMemoryAtom] = []
-        self._block_access_matrix: Dict[str, Dict[str, AccessPermission]] = {}
+        self._block_memory_type: MemoryType = block_memory_type
         
         if self._mem_block_id in self.__class__._memblock_instances_by_id.keys():
             raise ValueError(f"❌ Memory Block ID {self._mem_block_id} is already initiated.")
@@ -43,44 +43,35 @@ class AbstractMemoryBlock(ABC):
     def memory_atoms(self) -> List[AbstractMemoryAtom]:
         return self._memory_atoms
     @property
-    def block_access_matrix(self) -> Dict[str, Dict[str, AccessPermission]]:
-        return self._block_access_matrix
-    @property
     def mem_atom_graph(self) -> Dict[uuid.UUID, List[uuid.UUID]]:
         return self._mem_atom_graph
     
     @classmethod
-    def get_memblock_ids(cls, mem_block_id: uuid.UUID) -> List[uuid.UUID]:
+    def get_memblock_ids(cls) -> List[uuid.UUID]:
         return cls._list_of_memblock_ids
     @classmethod
     def get_memblock_instance_by_id(cls, mem_block_id: uuid.UUID) -> Self:
         return cls._memblock_instances_by_id[mem_block_id]
     
-    
     def add_memory_atom(self, memory_atom: AbstractMemoryAtom) -> None:
-        if memory_atom.mem_atom_id in [ma_id.mem_atom_id for ma_id in self._memory_atoms]:
-            raise ValueError(f"❌ Memory Atom with ID {memory_atom.mem_atom_id} had already existed in Memory Block {self._mem_block_id}.")
-        else:
-            self._memory_atoms.append(AbstractMemoryAtom.get_mematom_instance_by_id(memory_atom.mem_atom_id))
+        self._add_one_node_without_dependencies(memory_atom)
     
         for required_atom_id in memory_atom.required_atom:
-            if required_atom_id not in [ma_id.mem_atom_id for ma_id in self._memory_atoms]:
-                self._memory_atoms.append(AbstractMemoryAtom.get_mematom_instance_by_id(required_atom_id))
-                
+            self._add_one_node_without_dependencies(AbstractMemoryAtom.get_mematom_instance_by_id(required_atom_id))
             if required_atom_id not in self._mem_atom_graph[memory_atom.mem_atom_id]:
                 self._mem_atom_graph[memory_atom.mem_atom_id].append(required_atom_id)
                 
         for requiring_atom_id in memory_atom.requiring_atom:
-            if requiring_atom_id not in [ma_id.mem_atom_id for ma_id in self._memory_atoms]:
-                self._memory_atoms.append(AbstractMemoryAtom.get_mematom_instance_by_id(requiring_atom_id))
-                
+            self._add_one_node_without_dependencies(AbstractMemoryAtom.get_mematom_instance_by_id(requiring_atom_id))
             if requiring_atom_id not in self._mem_atom_graph[memory_atom.mem_atom_id]:
                 self._mem_atom_graph[memory_atom.mem_atom_id].append(requiring_atom_id)
     
-    @abstractmethod
-    def _get_permission(self, requester: SystemComponent, memory_atom: AbstractMemoryAtom) -> AccessPermission:
-        pass
-    
+    def _add_one_node_without_dependencies(self, memory_atom: AbstractMemoryAtom) -> None:
+        if memory_atom.mem_atom_id in [ma_id.mem_atom_id for ma_id in self._memory_atoms]:
+            raise ValueError(f"❌ Memory Atom with ID {memory_atom.mem_atom_id} had already existed in Memory Block {self._mem_block_id}.")
+        else:
+            self._memory_atoms.append(AbstractMemoryAtom.get_mematom_instance_by_id(memory_atom.mem_atom_id))
+                
     @abstractmethod
     def get_memory_atom(self, requester: SystemComponent, mem_atom_id: uuid.UUID) -> AbstractMemoryAtom:
         pass
