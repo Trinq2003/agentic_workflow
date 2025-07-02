b1af967d0ae65bc553b4f5a3f55209312d45fcdb
Trinq2003 <tri.nq210860@sis.hust.edu.vn>
2025-07-02 09:49:42 +0700

    Add react_operator test code

--- test_utils/react_operator.py
+++ test_utils/react_operator.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import asyncio
 
 # Configurations
 from configuration.llm_inference_configuration import APILLMConfiguration
@@ -40,18 +41,21 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()]
 )
 
+deepseek_configuration = APILLMConfiguration()
+deepseek_configuration.load("configuration/yaml/llm/deepseek.yaml")
+deepseek = RequestLLM(deepseek_configuration)
 
-gpt_4o_azure_configuration = APILLMConfiguration()
-gpt_4o_azure_configuration.load("configuration/yaml/llm/azure_gpt_4o.yaml")
-azure_gpt_4o = RequestLLM(gpt_4o_azure_configuration)
-
-text_embedding_ada_002_config = APIEmbeddingModelConfiguration()
-text_embedding_ada_002_config.load("configuration/yaml/embedding/text-embedding-ada-002.yaml")
-text_embedding_ada_002 = RequestEmbeddingModel(text_embedding_ada_002_config)
+embed_english_v3_config = APIEmbeddingModelConfiguration()
+embed_english_v3_config.load("configuration/yaml/embedding/embed-english-v3.yaml")
+embed_english_v3 = RequestEmbeddingModel(embed_english_v3_config)
 
 tool_chooser_tool_config = ToolChooserToolConfiguration()
 tool_chooser_tool_config.load("configuration/yaml/tools/tool_chooser.yaml")
 tool_chooser_tool = ToolChooserTool(tool_chooser_tool_config)
+
+react_demonstration_sampling_tool_config = DemonstrationSamplingToolConfiguration()
+react_demonstration_sampling_tool_config.load("configuration/yaml/tools/react_demonstration_sampling.yaml")
+react_demonstration_sampling_tool = DemonstrationSamplingTool(tool_config=react_demonstration_sampling_tool_config)
 
 react_operator_config = ReActOperatorConfiguration()
 react_operator_config.load("configuration/yaml/operators/react.yaml")
@@ -61,11 +65,32 @@ all_logger_names = list(logging.root.manager.loggerDict.keys())
 if '' not in all_logger_names:
     all_logger_names.append('')
 
+log_file_handler = logging.FileHandler('test_utils/logs/react_log.txt', mode='a+', encoding='utf-8')
+log_file_handler.setFormatter(logging.Formatter('%(asctime)s\t\t%(levelname)s\t%(message)s'))
+
 for logger_name in all_logger_names:
     logger = logging.getLogger(logger_name)
     if logger_name.startswith("Orbit"):
         logger.setLevel(logging.DEBUG)
+        logger.addHandler(log_file_handler)
     elif logger_name == '':
         logger.setLevel(logging.CRITICAL)
     else:
         logger.setLevel(logging.CRITICAL)
+
+
+react_test_message = UserMessagePrompt(
+    [
+        {
+            "role": "user",
+            "content": "Tell me the total number of running processes on my system, and their details. Call tools to get the information."
+        }
+    ]
+)
+
+react_test_answer, react_test_dependency_graph = asyncio.run(react_operator.run(input_message = react_test_message))
+print("*"*100)
+print(f"React test answer: {react_test_answer.prompt}")
+print("*"*100)
+print(f"React test dependency graph: {react_test_dependency_graph}")
+print("*"*100)
